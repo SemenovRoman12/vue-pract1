2e811d88774d52206bad9d3f8d373c8631e34de0
SemenovRoman12 <semenovroma305@gmail.com>
2025-02-10 13:05:21 +0700

    Add drag'n'drop and reviews filter

--- js/main.js
+++ js/main.js
@@ -2,40 +2,40 @@ let eventBus = new Vue();
 
 Vue.component('product-review', {
     template: `
-        <form class="review-form" @submit.prevent="onSubmit">
-            <p v-if="errors.length">
-                <b>Please correct the following errors:</b>
-                <ul>
-                    <li v-for="error in errors">{{ error }}</li>
-                </ul>
-            </p>
+    <form class="review-form" @submit.prevent="onSubmit">
+      <p v-if="errors.length">
+        <b>Please correct the following errors:</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
 
-            <p>
-                <label for="name">Name:</label>
-                <input id="name" v-model="name" placeholder="Your name">
-            </p>
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="Your name">
+      </p>
 
-            <p>
-                <label for="review">Review:</label>
-                <textarea id="review" v-model="review"></textarea>
-            </p>
+      <p>
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea>
+      </p>
 
-            <p>
-                <label for="rating">Rating:</label>
-                <select id="rating" v-model.number="rating">
-                    <option>5</option>
-                    <option>4</option>
-                    <option>3</option>
-                    <option>2</option>
-                    <option>1</option>
-                </select>
-            </p>
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
 
-            <p>
-                <input type="submit" value="Submit">
-            </p>
-        </form>
-    `,
+      <p>
+        <input type="submit" value="Submit">
+      </p>
+    </form>
+  `,
     data() {
         return {
             name: null,
@@ -74,35 +74,37 @@ Vue.component('product', {
         }
     },
     template: `
-        <div class="product">
-            <div class="product-image">
-                <img :src="image" :alt="altText">
-            </div>
+    <div class="product">
+      <div class="product-image">
+        <img :src="image" :alt="altText">
+      </div>
 
-            <div class="product-info">
-                <h1>{{ title }}</h1>
-                <p v-if="inStock">In Stock</p>
-                <p v-else>Out of Stock</p>
-                <ul>
-                    <li v-for="detail in details">{{ detail }}</li>
-                </ul>
-                <p>Shipping: {{ shipping }}</p>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <p>Shipping: {{ shipping }}</p>
 
-                <div class="color-box"
-                     v-for="(variant, index) in variants"
-                     :key="variant.variantId"
-                     :style="{ backgroundColor: variant.variantColor }"
-                     @mouseover="updateProduct(index)">
-                </div>
-
-                <button @click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
-                    Add to cart
-                </button>    
-            </div>
-
-            <product-tabs :reviews="reviews" :shipping="shipping" :details="details"></product-tabs>
+        <div class="color-box"
+             v-for="(variant, index) in variants"
+             :key="variant.variantId"
+             :style="{ backgroundColor: variant.variantColor }"
+             @mouseover="updateProduct(index)"
+             draggable="true"
+             @dragstart="dragStart($event, variant.variantId)">
         </div>
-    `,
+
+        <button @click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
+          Add to cart
+        </button>    
+      </div>
+
+      <product-tabs :reviews="reviews" :shipping="shipping" :details="details"></product-tabs>
+    </div>
+  `,
     data() {
         return {
             product: "Socks",
@@ -133,6 +135,9 @@ Vue.component('product', {
         },
         updateProduct(index) {
             this.selectedVariant = index;
+        },
+        dragStart(event, variantId) {
+            event.dataTransfer.setData('variantId', variantId);
         }
     },
     computed: {
@@ -172,48 +177,62 @@ Vue.component('product-tabs', {
         }
     },
     template: `
-        <div>
-            <ul>
-                <span class="tab"
-                      :class="{ activeTab: selectedTab === tab }"
-                      v-for="(tab, index) in tabs"
-                      :key="index"
-                      @click="selectedTab = tab">
-                    {{ tab }}
-                </span>
-            </ul>
+    <div>
+      <ul>
+        <span class="tab"
+              :class="{ activeTab: selectedTab === tab }"
+              v-for="(tab, index) in tabs"
+              :key="index"
+              @click="selectedTab = tab">
+          {{ tab }}
+        </span>
+      </ul>
 
-            <div v-show="selectedTab === 'Reviews'">
-                <p v-if="!reviews.length">There are no reviews yet.</p>
-                <ul>
-                    <li v-for="review in reviews">
-                        <p>{{ review.name }}</p>
-                        <p>Rating: {{ review.rating }}</p>
-                        <p>{{ review.review }}</p>
-                    </li>
-                </ul>
-            </div>
-
-            <div v-show="selectedTab === 'Make a Review'">
-                <product-review></product-review>
-            </div>
-
-            <div v-show="selectedTab === 'Shipping'">
-                <p>Shipping Cost: {{ shipping }}</p>
-            </div>
-
-            <div v-show="selectedTab === 'Details'">
-                <ul>
-                    <li v-for="detail in details">{{ detail }}</li>
-                </ul>
-            </div>
+      <div v-show="selectedTab === 'Reviews'">
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <div v-else>
+          <label for="filterRating">Filter by Rating:</label>
+          <select id="filterRating" v-model.number="filterRating">
+            <option :value="0">All</option>
+            <option v-for="n in [5,4,3,2,1]" :value="n">{{ n }}</option>
+          </select>
         </div>
-    `,
+        <ul>
+          <li v-for="review in filteredReviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+          </li>
+        </ul>
+      </div>
+
+      <div v-show="selectedTab === 'Make a Review'">
+        <product-review></product-review>
+      </div>
+
+      <div v-show="selectedTab === 'Shipping'">
+        <p>Shipping Cost: {{ shipping }}</p>
+      </div>
+
+      <div v-show="selectedTab === 'Details'">
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+      </div>
+    </div>
+  `,
     data() {
         return {
             tabs: ["Reviews", "Make a Review", "Shipping", "Details"],
-            selectedTab: "Reviews"
+            selectedTab: "Reviews",
+            filterRating: 0
         };
+    },
+    computed: {
+        filteredReviews() {
+            if (this.filterRating === 0) return this.reviews;
+            return this.reviews.filter(review => review.rating === this.filterRating);
+        }
     }
 });
 
@@ -225,7 +244,13 @@ let app = new Vue({
     },
     methods: {
         updateCart(id) {
-            this.cart.push(id);
+            this.cart.push(Number(id));
+        },
+        dropToCart(event) {
+            const variantId = event.dataTransfer.getData('variantId');
+            if (variantId) {
+                this.updateCart(variantId);
+            }
         }
     }
 });
